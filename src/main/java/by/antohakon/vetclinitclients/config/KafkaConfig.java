054d0f878784471f62c84012ed1cb7a16c615042
AntonP17 <pokrovskii.anton@mail.ru>
2025-08-12 10:35:23 +0300

    add producer config

--- src/main/java/by/antohakon/vetclinitclients/config/KafkaConfig.java
+++ src/main/java/by/antohakon/vetclinitclients/config/KafkaConfig.java
@@ -1,5 +1,6 @@
 package by.antohakon.vetclinitclients.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
@@ -19,6 +21,14 @@ public class KafkaConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
+
+    @Bean
+    public NewTopic clientsTopic() {
+        return TopicBuilder.name("client_visit_response")
+                .partitions(5)
+                .replicas(1)
+                .build();
+    }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> animalsOwnersKafkaListenerContainerFactory() {
--- src/main/java/by/antohakon/vetclinitclients/event/MyProducer.java
+++ src/main/java/by/antohakon/vetclinitclients/event/MyProducer.java
@@ -1,4 +1,19 @@
 package by.antohakon.vetclinitclients.event;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class MyProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public static void sendResponse(){
+
+    }
+
 }
